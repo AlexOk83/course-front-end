12e8a5bb4e9a9989df9a369c6fc5f4599997d07e
Okatyev Alexander <a.okatyev@bssys.com>
2019-02-25 02:19:12 +0400

    лендинг 1.  сделал переход к кнопкам

--- build/js/main.js
+++ build/js/main.js
@@ -38,6 +38,24 @@ jQuery(document).ready(function () {
     }
 
   });
+
+  $('.header__arrow-down').on('click', function(event) {
+    // отменяем стандартное действие
+    event.preventDefault();
+
+    var sc = $(this).attr("href"),
+      dn = $(sc).offset().top;
+    /*
+     * sc - в переменную заносим информацию о том, к какому блоку надо перейти
+     * dn - определяем положение блока на странице
+     */
+
+    $('html, body').animate({scrollTop: dn}, 1000);
+
+    /*
+     * 1000 скорость перехода в миллисекундах
+     */
+  });
 });
 
 /*
